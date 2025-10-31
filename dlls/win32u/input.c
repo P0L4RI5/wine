aa8bf1bc265273ab33eca2f42705da489c45409a
Paweł Kołodziejski <aquadran@gmail.com>
2025-10-31 09:21:14 +0100

    silence various logs

--- dlls/win32u/input.c
+++ dlls/win32u/input.c
@@ -1421,7 +1421,7 @@ HKL WINAPI NtUserActivateKeyboardLayout( HKL layout, UINT flags )
 
     TRACE_(keyboard)( "layout %p, flags %x\n", layout, flags );
 
-    if (flags) FIXME_(keyboard)( "flags %x not supported\n", flags );
+    //if (flags) FIXME_(keyboard)( "flags %x not supported\n", flags );
 
     if (layout == (HKL)HKL_NEXT || layout == (HKL)HKL_PREV)
     {
@@ -1434,7 +1434,7 @@ HKL WINAPI NtUserActivateKeyboardLayout( HKL layout, UINT flags )
         (NtQueryDefaultLocale( TRUE, &locale ) || LOWORD(layout) != locale))
     {
         RtlSetLastWin32Error( ERROR_CALL_NOT_IMPLEMENTED );
-        FIXME_(keyboard)( "Changing user locale is not supported\n" );
+        //FIXME_(keyboard)( "Changing user locale is not supported\n" );
         return 0;
     }
 
